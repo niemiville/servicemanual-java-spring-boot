013fdda1ba7bf96bec5d77745d36e375f3cb3b5b
Ville Niemi <vilsuxn@gmail.com>
2023-02-26 21:47:50 +0200

    save operation for factory device

--- src/main/java/com/etteplan/servicemanual/factorydevice/FactoryDeviceController.java
+++ src/main/java/com/etteplan/servicemanual/factorydevice/FactoryDeviceController.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
@@ -26,4 +29,11 @@ public class FactoryDeviceController {
         return repository.findById(id)
             .orElseThrow(() -> new FactoryDeviceNotFoundException(id));
     }
+    
+	@PostMapping("/factorydevices") 
+	FactoryDevice saveFactoryDevice(
+	  @Validated @RequestBody FactoryDevice device) { 
+		return repository.save(device); 
+	}
+	
 }
