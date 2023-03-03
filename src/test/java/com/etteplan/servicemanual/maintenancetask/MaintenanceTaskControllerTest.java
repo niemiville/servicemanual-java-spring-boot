349437ef392ed709aff6c26acd7c8a11df5e873f
Ville Niemi <vilsuxn@gmail.com>
2023-03-03 20:58:25 +0200

    Added MaintenanceTaskController test with two tests

---
+++ src/test/java/com/etteplan/servicemanual/maintenancetask/MaintenanceTaskControllerTest.java
@@ -0,0 +1,34 @@
+package com.etteplan.servicemanual.maintenancetask;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MaintenanceTaskControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getMaintenanceTasks() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/maintenancetasks").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getMaintenanceTaskNotFound() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/maintenancetasks/1000000").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound());
+    }
+}
